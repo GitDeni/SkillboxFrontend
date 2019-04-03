f47fd86f4a93cc449d4d8e24a854884d0400c7ae
Deniza <denidelippa@gmail.com>
2019-04-03 23:14:15 +0300

    full adaptive

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ $(document).ready(function(){
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: false
       }
     }
     // You can unslick at a given breakpoint now by adding:
@@ -42,31 +43,3 @@ $(document).ready(function(){
 
   
 });
-
-
-
-function draw() {
-  var canvas = document.getElementById('myCanvas');
-  if (canvas.getContext){
-	var ctx = canvas.getContext('2d');
-	var gradient = ctx.createLinearGradient(0, 0, 0, 290);
-	gradient.addColorStop(0, "#ff2f96");
-	gradient.addColorStop(1, "#ff8d5c");
-
-	// Cubic curves example
-	ctx.beginPath();
-	ctx.moveTo(7,210);
-	ctx.bezierCurveTo(15,125,103,16,215,10);
-	ctx.bezierCurveTo(290,6,350,46,352,148);
-	ctx.bezierCurveTo(345,240,290,287,165,285);
-	ctx.bezierCurveTo(63,288,5,260,7,210);
-	ctx.strokeStyle = gradient;
-	ctx.fillStyle = gradient;
-
-
-	ctx.stroke();
-	ctx.fill();
-  }
-}
-
-document.addEventListener("DOMContentLoaded", draw);
